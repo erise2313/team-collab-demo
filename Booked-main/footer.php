bb050fc05ffee9f1f4152c95de70901c07f35582
JEFiguracion <f1rain554@gmail.com>
2025-11-14 12:31:53 +0800

    Add files via upload

---
+++ Booked-main/footer.php
@@ -0,0 +1,32 @@
+<section class="footer">
+  <div class="footer_box_container">
+    <div class="footer_box">
+      <div class="footer_logo_cont">
+        <img src="book_logo.png" alt="">
+      <a href="home.php" class="book_logo">Booked</a>
+
+      </div>
+      <p><i class="fas fa-phone"></i> 870-000</p>
+      <p><i class="fas fa-envelope"></i> booked@gmail.com</p>
+      <p><i class="fas fa-map-marker-alt"></i> Gen Tri, Philippines - 4107</p>
+      <p><i class="fa-solid fa-shop"></i> Online Hours 24/7</p>
+    </div>
+
+    <div class="footer_box">
+      <h3>Quick Links</h3>
+      <a href="home.php">Home</a>
+      <a href="about.php">About</a>
+      <a href="shop.php">Shop</a>
+      <a href="contact.php">Contact</a>
+    </div>
+
+    <div class="footer_box">
+      <h3>Other Links</h3>
+      <a href="login.php">Login</a>
+      <a href="register.php">Register</a>
+      <a href="cart.php">Cart</a>
+      <a href="orders.php">Orders</a>
+    </div>
+  </div>
+  <p>Copyright <i class="fa-regular fa-copyright"></i> 2025 <span>BSIT201.CO | All Rights Reserved.</span></p>
+</section>
